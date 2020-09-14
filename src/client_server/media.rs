aa5e9e607ecc739d0d991ea7221dadd0125f6d64
Timo Kösters <timo@koesters.xyz>
2020-09-14 14:20:38 +0200

    feat: download media and thumbnails over federation

--- src/client_server/media.rs
+++ src/client_server/media.rs
@@ -1,5 +1,7 @@
 use super::State;
-use crate::{database::media::FileMeta, utils, ConduitResult, Database, Error, Ruma};
+use crate::{
+    database::media::FileMeta, server_server, utils, ConduitResult, Database, Error, Ruma,
+};
 use ruma::api::client::{
     error::ErrorKind,
     r0::media::{create_content, get_content, get_content_thumbnail, get_media_config},
@@ -35,7 +37,7 @@ pub fn create_content_route(
         utils::random_string(MXC_LENGTH)
     );
     db.media
-        .create(mxc.clone(), &body.filename, &body.content_type, &body.file)?;
+        .create(mxc.clone(), &body.filename.as_deref(), &body.content_type, &body.file)?;
 
     Ok(create_content::Response { content_uri: mxc }.into())
 }
@@ -47,19 +49,25 @@ pub fn create_content_route(
         data = "<body>"
     )
 )]
-pub fn get_content_route(
+pub async fn get_content_route(
     db: State<'_, Database>,
     body: Ruma<get_content::Request<'_>>,
     _server_name: String,
     _media_id: String,
 ) -> ConduitResult<get_content::Response> {
+        let mxc = format!(
+            "mxc://{}/{}",
+            db.globals.server_name(),
+            utils::random_string(MXC_LENGTH)
+        );
+
     if let Some(FileMeta {
         filename,
         content_type,
         file,
     }) = db
         .media
-        .get(format!("mxc://{}/{}", body.server_name, body.media_id))?
+        .get(&mxc)?
     {
         Ok(get_content::Response {
             file,
@@ -67,6 +75,26 @@ pub fn get_content_route(
             content_disposition: filename.unwrap_or_default(), // TODO: Spec says this should be optional
         }
         .into())
+    } else if body.allow_remote {
+        let get_content_response = server_server::send_request(
+            &db,
+            body.server_name.as_ref(),
+            get_content::Request {
+                allow_remote: false,
+                server_name: &body.server_name,
+                media_id: &body.media_id,
+            },
+        )
+        .await?;
+
+        db.media.create(
+            mxc,
+            &Some(&get_content_response.content_disposition),
+            &get_content_response.content_type,
+            &get_content_response.file,
+        )?;
+
+        Ok(get_content_response.into())
     } else {
         Err(Error::BadRequest(ErrorKind::NotFound, "Media not found."))
     }
@@ -79,7 +107,7 @@ pub fn get_content_route(
         data = "<body>"
     )
 )]
-pub fn get_content_thumbnail_route(
+pub async fn get_content_thumbnail_route(
     db: State<'_, Database>,
     body: Ruma<get_content_thumbnail::Request<'_>>,
     _server_name: String,
@@ -97,6 +125,37 @@ pub fn get_content_thumbnail_route(
             .map_err(|_| Error::BadRequest(ErrorKind::InvalidParam, "Width is invalid."))?,
     )? {
         Ok(get_content_thumbnail::Response { file, content_type }.into())
+    } else if body.allow_remote {
+        let get_thumbnail_response = server_server::send_request(
+            &db,
+            body.server_name.as_ref(),
+            get_content_thumbnail::Request {
+                allow_remote: false,
+                height: body.height,
+                width: body.width,
+                method: body.method,
+                server_name: &body.server_name,
+                media_id: &body.media_id,
+            },
+        )
+        .await?;
+
+        let mxc = format!(
+            "mxc://{}/{}",
+            db.globals.server_name(),
+            utils::random_string(MXC_LENGTH)
+        );
+
+        db.media.upload_thumbnail(
+            mxc,
+            &None,
+            &get_thumbnail_response.content_type,
+            body.width.try_into().expect("all UInts are valid u32s"),
+            body.height.try_into().expect("all UInts are valid u32s"),
+            &get_thumbnail_response.file,
+        )?;
+
+        Ok(get_thumbnail_response.into())
     } else {
         Err(Error::BadRequest(ErrorKind::NotFound, "Media not found."))
     }
