nekrondev/conduit_rockdb

Use Arc for EventIds in PDUs
Upgrades Ruma again to make this work.

src/client_server/redact.rs
@@ -17,14 +17,15 @@ use serde_json::value::to_raw_value;
 /// - TODO: Handle txn id
 #[cfg_attr(
     feature = "conduit_bin",
-    put("/_matrix/client/r0/rooms/<_>/redact/<_>/<_>", data = "<body>")
+    put("/_matrix/client/r0/rooms/<_>/redact/<_>/<_>", data = "<req>")
 )]
-#[tracing::instrument(skip(db, body))]
+#[tracing::instrument(skip(db, req))]
 pub async fn redact_event_route(
     db: DatabaseGuard,
-    body: Ruma<redact_event::Request<'_>>,
+    req: Ruma<redact_event::Request<'_>>,
 ) -> ConduitResult<redact_event::Response> {
-    let sender_user = body.sender_user.as_ref().expect("user is authenticated");
+    let body = req.body;
+    let sender_user = req.sender_user.as_ref().expect("user is authenticated");
 
     let mutex_state = Arc::clone(
         db.globals
@@ -45,7 +46,7 @@ pub async fn redact_event_route(
             .expect("event is valid, we just created it"),
             unsigned: None,
             state_key: None,
-            redacts: Some(body.event_id.clone()),
+            redacts: Some(body.event_id.into()),
         },
         sender_user,
         &body.room_id,
@@ -57,5 +58,6 @@ pub async fn redact_event_route(
 
     db.flush()?;
 
+    let event_id = (*event_id).to_owned();
     Ok(redact_event::Response { event_id }.into())
 }

girlbossceo/conduwuit

src/client_server/redact.rs
@@ -25,6 +25,7 @@ pub async fn redact_event_route(
     body: Ruma<redact_event::Request<'_>>,
 ) -> ConduitResult<redact_event::Response> {
     let sender_user = body.sender_user.as_ref().expect("user is authenticated");
+    let body = body.body;
 
     let mutex_state = Arc::clone(
         db.globals
@@ -45,7 +46,7 @@ pub async fn redact_event_route(
             .expect("event is valid, we just created it"),
             unsigned: None,
             state_key: None,
-            redacts: Some(body.event_id.clone()),
+            redacts: Some(body.event_id.into()),
         },
         sender_user,
         &body.room_id,
@@ -57,5 +58,6 @@ pub async fn redact_event_route(
 
     db.flush()?;
 
+    let event_id = (*event_id).to_owned();
     Ok(redact_event::Response { event_id }.into())
 }