974c10e739b70c5798450f5e51819e9d2beed5d3
Jonas Platte <jplatte+git@posteo.de>
2022-02-03 13:30:04 +0100

    Move Config out of database module

--- src/database.rs
+++ src/database.rs
@@ -6,7 +6,6 @@ pub mod appservice;
 pub mod globals;
 pub mod key_backups;
 pub mod media;
-pub mod proxy;
 pub mod pusher;
 pub mod rooms;
 pub mod sending;
@@ -14,7 +13,7 @@ pub mod transaction_ids;
 pub mod uiaa;
 pub mod users;
 
-use crate::{utils, Error, Result};
+use crate::{utils, Config, Error, Result};
 use abstraction::DatabaseEngine;
 use directories::ProjectDirs;
 use lru_cache::LruCache;
@@ -24,8 +23,7 @@ use rocket::{
     request::{FromRequest, Request},
     Shutdown, State,
 };
-use ruma::{DeviceId, EventId, RoomId, ServerName, UserId};
-use serde::{de::IgnoredAny, Deserialize};
+use ruma::{DeviceId, EventId, RoomId, UserId};
 use std::{
     collections::{BTreeMap, HashMap, HashSet},
     fs::{self, remove_dir_all},
@@ -37,130 +35,6 @@ use std::{
 };
 use tokio::sync::{OwnedRwLockReadGuard, RwLock as TokioRwLock, Semaphore};
 use tracing::{debug, error, warn};
-
-use self::proxy::ProxyConfig;
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct Config {
-    server_name: Box<ServerName>,
-    #[serde(default = "default_database_backend")]
-    database_backend: String,
-    database_path: String,
-    #[serde(default = "default_db_cache_capacity_mb")]
-    db_cache_capacity_mb: f64,
-    #[serde(default = "default_conduit_cache_capacity_modifier")]
-    conduit_cache_capacity_modifier: f64,
-    #[serde(default = "default_rocksdb_max_open_files")]
-    rocksdb_max_open_files: i32,
-    #[serde(default = "default_pdu_cache_capacity")]
-    pdu_cache_capacity: u32,
-    #[serde(default = "default_cleanup_second_interval")]
-    cleanup_second_interval: u32,
-    #[serde(default = "default_max_request_size")]
-    max_request_size: u32,
-    #[serde(default = "default_max_concurrent_requests")]
-    max_concurrent_requests: u16,
-    #[serde(default = "false_fn")]
-    allow_registration: bool,
-    #[serde(default = "true_fn")]
-    allow_encryption: bool,
-    #[serde(default = "false_fn")]
-    allow_federation: bool,
-    #[serde(default = "true_fn")]
-    allow_room_creation: bool,
-    #[serde(default = "false_fn")]
-    pub allow_jaeger: bool,
-    #[serde(default = "false_fn")]
-    pub tracing_flame: bool,
-    #[serde(default)]
-    proxy: ProxyConfig,
-    jwt_secret: Option<String>,
-    #[serde(default = "Vec::new")]
-    trusted_servers: Vec<Box<ServerName>>,
-    #[serde(default = "default_log")]
-    pub log: String,
-    #[serde(default)]
-    turn_username: String,
-    #[serde(default)]
-    turn_password: String,
-    #[serde(default = "Vec::new")]
-    turn_uris: Vec<String>,
-    #[serde(default)]
-    turn_secret: String,
-    #[serde(default = "default_turn_ttl")]
-    turn_ttl: u64,
-
-    #[serde(flatten)]
-    catchall: BTreeMap<String, IgnoredAny>,
-}
-
-const DEPRECATED_KEYS: &[&str] = &["cache_capacity"];
-
-impl Config {
-    pub fn warn_deprecated(&self) {
-        let mut was_deprecated = false;
-        for key in self
-            .catchall
-            .keys()
-            .filter(|key| DEPRECATED_KEYS.iter().any(|s| s == key))
-        {
-            warn!("Config parameter {} is deprecated", key);
-            was_deprecated = true;
-        }
-
-        if was_deprecated {
-            warn!("Read conduit documentation and check your configuration if any new configuration parameters should be adjusted");
-        }
-    }
-}
-
-fn false_fn() -> bool {
-    false
-}
-
-fn true_fn() -> bool {
-    true
-}
-
-fn default_database_backend() -> String {
-    "sqlite".to_owned()
-}
-
-fn default_db_cache_capacity_mb() -> f64 {
-    10.0
-}
-
-fn default_conduit_cache_capacity_modifier() -> f64 {
-    1.0
-}
-
-fn default_rocksdb_max_open_files() -> i32 {
-    20
-}
-
-fn default_pdu_cache_capacity() -> u32 {
-    150_000
-}
-
-fn default_cleanup_second_interval() -> u32 {
-    1 * 60 // every minute
-}
-
-fn default_max_request_size() -> u32 {
-    20 * 1024 * 1024 // Default to 20 MB
-}
-
-fn default_max_concurrent_requests() -> u16 {
-    100
-}
-
-fn default_log() -> String {
-    "info,state_res=warn,rocket=off,_=off,sled=off".to_owned()
-}
-
-fn default_turn_ttl() -> u64 {
-    60 * 60 * 24
-}
 
 pub struct Database {
     _db: Arc<dyn DatabaseEngine>,
