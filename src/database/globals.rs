timokoesters/conduit

Enable room version 4

src/database/globals.rs
@@ -138,7 +138,7 @@ impl Globals {
         // Supported and stable room versions
         let stable_room_versions = vec![RoomVersionId::Version6];
         // Experimental, partially supported room versions
-        let unstable_room_versions = vec![RoomVersionId::Version5];
+        let unstable_room_versions = vec![RoomVersionId::Version4, RoomVersionId::Version5];
 
         let s = Self {
             globals,

girlbossceo/conduwuit

src/database/globals.rs
@@ -151,7 +151,7 @@ impl Globals {
         // Supported and stable room versions
         let stable_room_versions = vec![RoomVersionId::V6];
         // Experimental, partially supported room versions
-        let unstable_room_versions = vec![RoomVersionId::V5];
+        let unstable_room_versions = vec![RoomVersionId::V4, RoomVersionId::V5];
 
         let s = Self {
             globals,