timokoesters/conduit

Refactor room version support, add default room version config

src/client_server/capabilities.rs
@@ -1,9 +1,6 @@
-use crate::{ConduitResult, Ruma};
-use ruma::{
-    api::client::r0::capabilities::{
-        get_capabilities, Capabilities, RoomVersionStability, RoomVersionsCapability,
-    },
-    RoomVersionId,
+use crate::{database::DatabaseGuard, ConduitResult, Ruma};
+use ruma::api::client::r0::capabilities::{
+    get_capabilities, Capabilities, RoomVersionStability, RoomVersionsCapability,
 };
 use std::collections::BTreeMap;
 
@@ -17,17 +14,28 @@ use rocket::get;
     feature = "conduit_bin",
     get("/_matrix/client/r0/capabilities", data = "<_body>")
 )]
-#[tracing::instrument(skip(_body))]
+#[tracing::instrument(skip(_body, db))]
 pub async fn get_capabilities_route(
     _body: Ruma<get_capabilities::Request>,
+    db: DatabaseGuard,
 ) -> ConduitResult<get_capabilities::Response> {
     let mut available = BTreeMap::new();
-    available.insert(RoomVersionId::Version5, RoomVersionStability::Stable);
-    available.insert(RoomVersionId::Version6, RoomVersionStability::Stable);
+    if db.globals.allow_unstable_room_versions() {
+        for room_version in &db.globals.unstable_room_versions {
+            available.insert(room_version.clone(), RoomVersionStability::Stable);
+        }
+    } else {
+        for room_version in &db.globals.unstable_room_versions {
+            available.insert(room_version.clone(), RoomVersionStability::Unstable);
+        }
+    }
+    for room_version in &db.globals.stable_room_versions {
+        available.insert(room_version.clone(), RoomVersionStability::Stable);
+    }
 
     let mut capabilities = Capabilities::new();
     capabilities.room_versions = RoomVersionsCapability {
-        default: RoomVersionId::Version6,
+        default: db.globals.default_room_version(),
         available,
     };
 

src/database/globals.rs
@@ -4,7 +4,8 @@ use ruma::{
         client::r0::sync::sync_events,
         federation::discovery::{ServerSigningKeys, VerifyKey},
     },
-    DeviceId, EventId, MilliSecondsSinceUnixEpoch, RoomId, ServerName, ServerSigningKeyId, UserId,
+    DeviceId, EventId, MilliSecondsSinceUnixEpoch, RoomId, RoomVersionId, ServerName,
+    ServerSigningKeyId, UserId,
 };
 use std::{
     collections::{BTreeMap, HashMap},
@@ -39,6 +40,8 @@ pub struct Globals {
     keypair: Arc<ruma::signatures::Ed25519KeyPair>,
     dns_resolver: TokioAsyncResolver,
     jwt_decoding_key: Option<jsonwebtoken::DecodingKey<'static>>,
+    pub stable_room_versions: Vec<RoomVersionId>,
+    pub unstable_room_versions: Vec<RoomVersionId>,
     pub(super) server_signingkeys: Arc<dyn Tree>,
     pub bad_event_ratelimiter: Arc<RwLock<HashMap<EventId, RateLimitState>>>,
     pub bad_signature_ratelimiter: Arc<RwLock<HashMap<Vec<String>, RateLimitState>>>,
@@ -132,6 +135,11 @@ impl Globals {
             .as_ref()
             .map(|secret| jsonwebtoken::DecodingKey::from_secret(secret.as_bytes()).into_static());
 
+        // Supported and stable room versions
+        let stable_room_versions = vec![RoomVersionId::Version6];
+        // Experimental, partially supported room versions
+        let unstable_room_versions = vec![RoomVersionId::Version5];
+
         let s = Self {
             globals,
             config,
@@ -143,6 +151,8 @@ impl Globals {
             tls_name_override,
             server_signingkeys,
             jwt_decoding_key,
+            stable_room_versions,
+            unstable_room_versions,
             bad_event_ratelimiter: Arc::new(RwLock::new(HashMap::new())),
             bad_signature_ratelimiter: Arc::new(RwLock::new(HashMap::new())),
             servername_ratelimiter: Arc::new(RwLock::new(HashMap::new())),
@@ -214,6 +224,22 @@ impl Globals {
         self.config.allow_room_creation
     }
 
+    pub fn allow_unstable_room_versions(&self) -> bool {
+        self.config.allow_unstable_room_versions
+    }
+
+    pub fn default_room_version(&self) -> RoomVersionId {
+        if self
+            .supported_room_versions()
+            .contains(&self.config.default_room_version.clone())
+        {
+            self.config.default_room_version.clone()
+        } else {
+            error!("Room version in config isn't supported, falling back to Version 6");
+            RoomVersionId::Version6
+        }
+    }
+
     pub fn trusted_servers(&self) -> &[Box<ServerName>] {
         &self.config.trusted_servers
     }
@@ -244,6 +270,19 @@ impl Globals {
 
     pub fn turn_secret(&self) -> &String {
         &self.config.turn_secret
+    }
+
+    pub fn supported_room_versions(&self) -> Vec<RoomVersionId> {
+        let mut room_versions: Vec<RoomVersionId> = vec![];
+        self.stable_room_versions
+            .iter()
+            .for_each(|room_version| room_versions.push(room_version.clone()));
+        if self.allow_unstable_room_versions() {
+            self.unstable_room_versions
+                .iter()
+                .for_each(|room_version| room_versions.push(room_version.clone()));
+        };
+        room_versions
     }
 
     /// TODO: the key valid until timestamp is only honored in room version > 4

girlbossceo/conduwuit

src/client_server/capabilities.rs
@@ -1,9 +1,8 @@
-use crate::{Result, Ruma};
+use crate::{database::DatabaseGuard, Result, Ruma};
 use ruma::{
     api::client::discovery::get_capabilities::{
         self, Capabilities, RoomVersionStability, RoomVersionsCapability,
     },
-    RoomVersionId,
 };
 use std::collections::BTreeMap;
 
@@ -11,15 +10,26 @@ use std::collections::BTreeMap;
 ///
 /// Get information on the supported feature set and other relevent capabilities of this server.
 pub async fn get_capabilities_route(
+    db: DatabaseGuard,
     _body: Ruma<get_capabilities::v3::IncomingRequest>,
 ) -> Result<get_capabilities::v3::Response> {
     let mut available = BTreeMap::new();
-    available.insert(RoomVersionId::V5, RoomVersionStability::Stable);
-    available.insert(RoomVersionId::V6, RoomVersionStability::Stable);
+    if db.globals.allow_unstable_room_versions() {
+        for room_version in &db.globals.unstable_room_versions {
+            available.insert(room_version.clone(), RoomVersionStability::Stable);
+        }
+    } else {
+        for room_version in &db.globals.unstable_room_versions {
+            available.insert(room_version.clone(), RoomVersionStability::Unstable);
+        }
+    }
+    for room_version in &db.globals.stable_room_versions {
+        available.insert(room_version.clone(), RoomVersionStability::Stable);
+    }
 
     let mut capabilities = Capabilities::new();
     capabilities.room_versions = RoomVersionsCapability {
-        default: RoomVersionId::V6,
+        default: db.globals.default_room_version(),
         available,
     };
 

src/database/globals.rs
@@ -4,7 +4,8 @@ use ruma::{
         client::sync::sync_events,
         federation::discovery::{ServerSigningKeys, VerifyKey},
     },
-    DeviceId, EventId, MilliSecondsSinceUnixEpoch, RoomId, ServerName, ServerSigningKeyId, UserId,
+    DeviceId, EventId, MilliSecondsSinceUnixEpoch, RoomId, RoomVersionId, ServerName,
+    ServerSigningKeyId, UserId,
 };
 use std::{
     collections::{BTreeMap, HashMap},
@@ -41,6 +42,8 @@ pub struct Globals {
     jwt_decoding_key: Option<jsonwebtoken::DecodingKey<'static>>,
     federation_client: reqwest::Client,
     default_client: reqwest::Client,
+    pub stable_room_versions: Vec<RoomVersionId>,
+    pub unstable_room_versions: Vec<RoomVersionId>,
     pub(super) server_signingkeys: Arc<dyn Tree>,
     pub bad_event_ratelimiter: Arc<RwLock<HashMap<Box<EventId>, RateLimitState>>>,
     pub bad_signature_ratelimiter: Arc<RwLock<HashMap<Vec<String>, RateLimitState>>>,
@@ -145,6 +148,11 @@ impl Globals {
             })
             .build()?;
 
+        // Supported and stable room versions
+        let stable_room_versions = vec![RoomVersionId::V6];
+        // Experimental, partially supported room versions
+        let unstable_room_versions = vec![RoomVersionId::V5];
+
         let s = Self {
             globals,
             config,
@@ -162,6 +170,8 @@ impl Globals {
             default_client,
             server_signingkeys,
             jwt_decoding_key,
+            stable_room_versions,
+            unstable_room_versions,
             bad_event_ratelimiter: Arc::new(RwLock::new(HashMap::new())),
             bad_signature_ratelimiter: Arc::new(RwLock::new(HashMap::new())),
             servername_ratelimiter: Arc::new(RwLock::new(HashMap::new())),
@@ -232,6 +242,22 @@ impl Globals {
         self.config.allow_room_creation
     }
 
+    pub fn allow_unstable_room_versions(&self) -> bool {
+        self.config.allow_unstable_room_versions
+    }
+
+    pub fn default_room_version(&self) -> RoomVersionId {
+        if self
+            .supported_room_versions()
+            .contains(&self.config.default_room_version.clone())
+        {
+            self.config.default_room_version.clone()
+        } else {
+            error!("Room version in config isn't supported, falling back to Version 6");
+            RoomVersionId::V6
+        }
+    }
+
     pub fn trusted_servers(&self) -> &[Box<ServerName>] {
         &self.config.trusted_servers
     }
@@ -266,6 +292,19 @@ impl Globals {
 
     pub fn emergency_password(&self) -> &Option<String> {
         &self.config.emergency_password
+    }
+
+    pub fn supported_room_versions(&self) -> Vec<RoomVersionId> {
+        let mut room_versions: Vec<RoomVersionId> = vec![];
+        self.stable_room_versions
+            .iter()
+            .for_each(|room_version| room_versions.push(room_version.clone()));
+        if self.allow_unstable_room_versions() {
+            self.unstable_room_versions
+                .iter()
+                .for_each(|room_version| room_versions.push(room_version.clone()));
+        };
+        room_versions
     }
 
     /// TODO: the key valid until timestamp is only honored in room version > 4