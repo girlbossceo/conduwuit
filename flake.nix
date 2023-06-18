15e60818c908e58d53580e3fd049e452b2133610
Charles Hall <charles@computer.surgery>
2023-06-17 17:02:10 -0700

    pin nixos-unstable, update flake.lock
    `nixos-unstable` is the rolling release channel of NixOS. The default is
    the master branch, which doesn't always have a populated binary cache
    and so may result in compiling a bunch of stuff unnecessarily.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     fenix = {
