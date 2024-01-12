6c5b13b9db3da0758a12837521f9da74e23baa53
strawberry <strawberry@puppygock.gay>
2024-01-11 20:26:23 -0500

    declare explicit support for room filtering (MSC3827)
    conduit has supported this for a while now and is a Matrix 1.4 feature
    
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/api/client_server/unversioned.rs
+++ src/api/client_server/unversioned.rs
@@ -37,6 +37,7 @@ pub async fn get_supported_versions_route(
         unstable_features: BTreeMap::from_iter([
             ("org.matrix.e2e_cross_signing".to_owned(), true),
             ("org.matrix.msc2836".to_owned(), true),
+            ("org.matrix.msc3827".to_owned(), true),
             ("org.matrix.msc2946".to_owned(), true),
         ]),
     };
