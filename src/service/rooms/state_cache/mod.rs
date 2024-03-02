1327cd93f97266595471a56707f8d69b374d7884
strawberry <strawberry@puppygock.gay>
2024-03-02 17:26:37 -0500

    remove single-use lifetimes
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/service/rooms/state_cache/mod.rs
+++ src/service/rooms/state_cache/mod.rs
@@ -262,7 +262,7 @@ impl Service {
     }
 
     #[tracing::instrument(skip(self))]
-    pub fn server_in_room<'a>(&'a self, server: &ServerName, room_id: &RoomId) -> Result<bool> {
+    pub fn server_in_room(&self, server: &ServerName, room_id: &RoomId) -> Result<bool> {
         self.db.server_in_room(server, room_id)
     }
 
