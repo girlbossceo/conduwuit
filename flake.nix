cd2f00e012ec473d9ef17b27edd999e7ac982c86
strawberry <strawberry@puppygock.gay>
2024-03-09 15:45:27 -0500

    format flake, use rust-rocksdb fork
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- flake.nix
+++ flake.nix
@@ -37,13 +37,19 @@
     let
       pkgsHost = nixpkgs.legacyPackages.${system};
 
-      rocksdb' = pkgs: pkgs.rocksdb.overrideAttrs (old:
-              {
-                version = "8.10.0";
-                src = old.src.override {
-                  hash = "sha256-KGsYDBc1fz/90YYNGwlZ0LUKXYsP1zyhP29TnRQwgjQ=";
-                };
-              });
+      rocksdb' = pkgs:
+        let
+          version = "8.11.3";
+        in
+        pkgs.rocksdb.overrideAttrs (old: {
+          inherit version;
+          src = pkgs.fetchFromGitHub {
+            owner = "facebook";
+            repo = "rocksdb";
+            rev = "v${version}";
+            hash = "sha256-KGsYDBc1fz/90YYNGwlZ0LUKXYsP1zyhP29TnRQwgjQ=";
+          };
+        });
 
       # Nix-accessible `Cargo.toml`
       cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
@@ -75,38 +81,38 @@
       }
       // {
         CARGO_BUILD_RUSTFLAGS = let inherit (pkgs) lib stdenv; in
-          lib.concatStringsSep " " ([]
+          lib.concatStringsSep " " ([ ]
             ++ lib.optionals
-              # This disables PIE for static builds, which isn't great in terms
-              # of security. Unfortunately, my hand is forced because nixpkgs'
-              # `libstdc++.a` is built without `-fPIE`, which precludes us from
-              # leaving PIE enabled.
-              stdenv.hostPlatform.isStatic
-              ["-C" "relocation-model=static"]
+            # This disables PIE for static builds, which isn't great in terms
+            # of security. Unfortunately, my hand is forced because nixpkgs'
+            # `libstdc++.a` is built without `-fPIE`, which precludes us from
+            # leaving PIE enabled.
+            stdenv.hostPlatform.isStatic
+            [ "-C" "relocation-model=static" ]
             ++ lib.optionals
-              (stdenv.buildPlatform.config != stdenv.hostPlatform.config)
-              ["-l" "c"]
+            (stdenv.buildPlatform.config != stdenv.hostPlatform.config)
+            [ "-l" "c" ]
             ++ lib.optionals
-              # This check has to match the one [here][0]. We only need to set
-              # these flags when using a different linker. Don't ask me why,
-              # though, because I don't know. All I know is it breaks otherwise.
-              #
-              # [0]: https://github.com/NixOS/nixpkgs/blob/5cdb38bb16c6d0a38779db14fcc766bc1b2394d6/pkgs/build-support/rust/lib/default.nix#L37-L40
-              (
-                # Nixpkgs doesn't check for x86_64 here but we do, because I
-                # observed a failure building statically for x86_64 without
-                # including it here. Linkers are weird.
-                (stdenv.hostPlatform.isAarch64 || stdenv.hostPlatform.isx86_64)
-                  && stdenv.hostPlatform.isStatic
-                  && !stdenv.isDarwin
-                  && !stdenv.cc.bintools.isLLVM
-              )
-              [
-                "-l"
-                "stdc++"
-                "-L"
-                "${stdenv.cc.cc.lib}/${stdenv.hostPlatform.config}/lib"
-              ]
+            # This check has to match the one [here][0]. We only need to set
+            # these flags when using a different linker. Don't ask me why,
+            # though, because I don't know. All I know is it breaks otherwise.
+            #
+            # [0]: https://github.com/NixOS/nixpkgs/blob/5cdb38bb16c6d0a38779db14fcc766bc1b2394d6/pkgs/build-support/rust/lib/default.nix#L37-L40
+            (
+              # Nixpkgs doesn't check for x86_64 here but we do, because I
+              # observed a failure building statically for x86_64 without
+              # including it here. Linkers are weird.
+              (stdenv.hostPlatform.isAarch64 || stdenv.hostPlatform.isx86_64)
+                && stdenv.hostPlatform.isStatic
+                && !stdenv.isDarwin
+                && !stdenv.cc.bintools.isLLVM
+            )
+            [
+              "-l"
+              "stdc++"
+              "-L"
+              "${stdenv.cc.cc.lib}/${stdenv.hostPlatform.config}/lib"
+            ]
           );
       }
 
@@ -134,29 +140,30 @@
                 envVars.linkerForTarget;
             }
           )
-      // (
-        let
-          inherit (pkgs.stdenv.hostPlatform.rust) cargoEnvVarTarget rustcTarget;
-        in
-        {
-          "CC_${cargoEnvVarTarget}" = envVars.ccForHost;
-          "CXX_${cargoEnvVarTarget}" = envVars.cxxForHost;
-          "CARGO_TARGET_${cargoEnvVarTarget}_LINKER" = envVars.linkerForHost;
-          CARGO_BUILD_TARGET = rustcTarget;
-        }
-      )
-      // (
-        let
-          inherit (pkgs.stdenv.buildPlatform.rust) cargoEnvVarTarget;
-        in
-        {
-          "CC_${cargoEnvVarTarget}" = envVars.ccForBuild;
-          "CXX_${cargoEnvVarTarget}" = envVars.cxxForBuild;
-          "CARGO_TARGET_${cargoEnvVarTarget}_LINKER" = envVars.linkerForBuild;
-          HOST_CC = "${pkgs.pkgsBuildHost.stdenv.cc}/bin/cc";
-          HOST_CXX = "${pkgs.pkgsBuildHost.stdenv.cc}/bin/c++";
-        }
-      ));
+        // (
+          let
+            inherit (pkgs.stdenv.hostPlatform.rust) cargoEnvVarTarget rustcTarget;
+          in
+          {
+            "CC_${cargoEnvVarTarget}" = envVars.ccForHost;
+            "CXX_${cargoEnvVarTarget}" = envVars.cxxForHost;
+            "CARGO_TARGET_${cargoEnvVarTarget}_LINKER" = envVars.linkerForHost;
+            CARGO_BUILD_TARGET = rustcTarget;
+          }
+        )
+        // (
+          let
+            inherit (pkgs.stdenv.buildPlatform.rust) cargoEnvVarTarget;
+          in
+          {
+            "CC_${cargoEnvVarTarget}" = envVars.ccForBuild;
+            "CXX_${cargoEnvVarTarget}" = envVars.cxxForBuild;
+            "CARGO_TARGET_${cargoEnvVarTarget}_LINKER" = envVars.linkerForBuild;
+            HOST_CC = "${pkgs.pkgsBuildHost.stdenv.cc}/bin/cc";
+            HOST_CXX = "${pkgs.pkgsBuildHost.stdenv.cc}/bin/c++";
+          }
+        )
+      );
 
       package = pkgs: builder pkgs {
         src = nix-filter {
