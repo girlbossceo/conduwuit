d1188a08f1bafd8a7f4c50a368a805422778f7be
strawberry <strawberry@puppygock.gay>
2024-03-14 19:05:15 -0400

    Revert "switch to hickory-dns / hickory_resolver"
    This reverts commit 2ea524bfabeb20e3f2f523ca276fbb9782b6d9e9.

--- src/service/globals/mod.rs
+++ src/service/globals/mod.rs
@@ -17,7 +17,6 @@ use argon2::Argon2;
 use base64::{engine::general_purpose, Engine as _};
 pub use data::Data;
 use futures_util::FutureExt;
-use hickory_resolver::TokioAsyncResolver;
 use hyper::{
 	client::connect::dns::{GaiResolver, Name},
 	service::Service as HyperService,
@@ -35,6 +34,7 @@ use ruma::{
 };
 use tokio::sync::{broadcast, watch::Receiver, Mutex, RwLock, Semaphore};
 use tracing::{error, info};
+use trust_dns_resolver::TokioAsyncResolver;
 
 use crate::{api::server_server::FedDest, services, Config, Error, Result};
 
