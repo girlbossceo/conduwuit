15c3e03908c0ca66eedd4d1f0c8657212c279d4c
Jason Volk <jason@zemos.net>
2024-03-19 04:31:57 -0700

    add abstract fallbacks for kv batch methods.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/database/abstraction.rs
+++ src/database/abstraction.rs
@@ -39,20 +39,35 @@ pub(crate) trait KvTree: Send + Sync {
 	}
 
 	fn insert(&self, key: &[u8], value: &[u8]) -> Result<()>;
-	fn insert_batch(&self, iter: &mut dyn Iterator<Item = (Vec<u8>, Vec<u8>)>) -> Result<()>;
+	fn insert_batch(&self, iter: &mut dyn Iterator<Item = (Vec<u8>, Vec<u8>)>) -> Result<()> {
+		for (key, value) in iter {
+			self.insert(&key, &value)?;
+		}
+
+		Ok(())
+	}
 
 	fn remove(&self, key: &[u8]) -> Result<()>;
+	fn remove_batch(&self, iter: &mut dyn Iterator<Item = Vec<u8>>) -> Result<()> {
+		for key in iter {
+			self.remove(&key)?;
+		}
 
-	#[allow(dead_code)]
-	#[cfg(feature = "rocksdb")]
-	fn remove_batch(&self, _iter: &mut dyn Iterator<Item = Vec<u8>>) -> Result<()> { unimplemented!() }
+		Ok(())
+	}
 
 	fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = (Vec<u8>, Vec<u8>)> + 'a>;
 
 	fn iter_from<'a>(&'a self, from: &[u8], backwards: bool) -> Box<dyn Iterator<Item = (Vec<u8>, Vec<u8>)> + 'a>;
 
 	fn increment(&self, key: &[u8]) -> Result<Vec<u8>>;
-	fn increment_batch(&self, iter: &mut dyn Iterator<Item = Vec<u8>>) -> Result<()>;
+	fn increment_batch(&self, iter: &mut dyn Iterator<Item = Vec<u8>>) -> Result<()> {
+		for key in iter {
+			self.increment(&key)?;
+		}
+
+		Ok(())
+	}
 
 	fn scan_prefix<'a>(&'a self, prefix: Vec<u8>) -> Box<dyn Iterator<Item = (Vec<u8>, Vec<u8>)> + 'a>;
 
