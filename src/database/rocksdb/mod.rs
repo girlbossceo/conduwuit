26b98fa72e42cb37aad19f66dc443e7e41428ed8
Jason Volk <jason@zemos.net>
2024-04-06 08:48:41 -0700

    refactor rocksdb opts; split kvtree
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/database/rocksdb/mod.rs
+++ src/database/rocksdb/mod.rs
@@ -1,20 +1,22 @@
-use std::{
-	future::Future,
-	pin::Pin,
-	sync::{atomic::AtomicU32, Arc},
-};
+use std::sync::{atomic::AtomicU32, Arc};
 
 use chrono::{DateTime, Utc};
 use rust_rocksdb::{
 	backup::{BackupEngine, BackupEngineOptions},
-	DBWithThreadMode as Db,
-	LogLevel::{Debug, Error, Fatal, Info, Warn},
-	MultiThreaded, WriteBatchWithTransaction,
+	DBWithThreadMode as Db, MultiThreaded,
 };
 use tracing::{debug, error, info, warn};
 
 use super::{super::Config, watchers::Watchers, KeyValueDatabaseEngine, KvTree};
-use crate::{utils, Result};
+use crate::Result;
+
+pub(crate) mod kvtree;
+pub(crate) mod opts;
+
+use kvtree::RocksDbEngineTree;
+use opts::{cf_options, db_options};
+
+use super::watchers;
 
 pub(crate) struct Engine {
 	rocks: Db<MultiThreaded>,
@@ -25,113 +27,6 @@ pub(crate) struct Engine {
 	env: rust_rocksdb::Env,
 	config: Config,
 	corks: AtomicU32,
-}
-
-struct RocksDbEngineTree<'a> {
-	db: Arc<Engine>,
-	name: &'a str,
-	watchers: Watchers,
-}
-
-fn db_options(
-	config: &Config, env: &rust_rocksdb::Env, row_cache: &rust_rocksdb::Cache, col_cache: &rust_rocksdb::Cache,
-) -> rust_rocksdb::Options {
-	// database options: https://docs.rs/rocksdb/latest/rocksdb/struct.Options.html#
-	let mut db_opts = rust_rocksdb::Options::default();
-
-	// Logging
-	let rocksdb_log_level = match config.rocksdb_log_level.as_ref() {
-		"debug" => Debug,
-		"info" => Info,
-		"warn" => Warn,
-		"fatal" => Fatal,
-		_ => Error,
-	};
-	db_opts.set_log_level(rocksdb_log_level);
-	db_opts.set_max_log_file_size(config.rocksdb_max_log_file_size);
-	db_opts.set_log_file_time_to_roll(config.rocksdb_log_time_to_roll);
-	db_opts.set_keep_log_file_num(config.rocksdb_max_log_files);
-
-	// Processing
-	let threads = if config.rocksdb_parallelism_threads == 0 {
-		num_cpus::get_physical() // max cores if user specified 0
-	} else {
-		config.rocksdb_parallelism_threads
-	};
-
-	db_opts.set_max_background_jobs(threads.try_into().unwrap());
-	db_opts.set_max_subcompactions(threads.try_into().unwrap());
-
-	// IO
-	db_opts.set_manual_wal_flush(true);
-	db_opts.set_use_direct_reads(true);
-	db_opts.set_use_direct_io_for_flush_and_compaction(true);
-	if config.rocksdb_optimize_for_spinning_disks {
-		db_opts.set_skip_stats_update_on_db_open(true); // speeds up opening DB on hard
-		                                        // drives
-	}
-
-	// Blocks
-	let mut block_based_options = rust_rocksdb::BlockBasedOptions::default();
-	block_based_options.set_block_size(4 * 1024);
-	block_based_options.set_metadata_block_size(4 * 1024);
-	block_based_options.set_bloom_filter(9.6, true);
-	block_based_options.set_optimize_filters_for_memory(true);
-	block_based_options.set_cache_index_and_filter_blocks(true);
-	block_based_options.set_pin_top_level_index_and_filter(true);
-	block_based_options.set_block_cache(col_cache);
-	db_opts.set_row_cache(row_cache);
-
-	// Buffers
-	db_opts.set_write_buffer_size(2 * 1024 * 1024);
-	db_opts.set_max_write_buffer_number(2);
-	db_opts.set_min_write_buffer_number(1);
-
-	// Files
-	db_opts.set_level_zero_file_num_compaction_trigger(1);
-	db_opts.set_target_file_size_base(64 * 1024 * 1024);
-	db_opts.set_max_bytes_for_level_base(128 * 1024 * 1024);
-	db_opts.set_ttl(14 * 24 * 60 * 60);
-
-	// Compression
-	let rocksdb_compression_algo = match config.rocksdb_compression_algo.as_ref() {
-		"zlib" => rust_rocksdb::DBCompressionType::Zlib,
-		"lz4" => rust_rocksdb::DBCompressionType::Lz4,
-		"bz2" => rust_rocksdb::DBCompressionType::Bz2,
-		_ => rust_rocksdb::DBCompressionType::Zstd,
-	};
-
-	if config.rocksdb_bottommost_compression {
-		db_opts.set_bottommost_compression_type(rocksdb_compression_algo);
-		db_opts.set_bottommost_zstd_max_train_bytes(0, true);
-
-		// -14 w_bits is only read by zlib.
-		db_opts.set_bottommost_compression_options(-14, config.rocksdb_bottommost_compression_level, 0, 0, true);
-	}
-
-	// -14 w_bits is only read by zlib.
-	db_opts.set_compression_options(-14, config.rocksdb_compression_level, 0, 0);
-	db_opts.set_compression_type(rocksdb_compression_algo);
-
-	// Misc
-	db_opts.create_if_missing(true);
-
-	// Default: https://github.com/facebook/rocksdb/wiki/WAL-Recovery-Modes#ktoleratecorruptedtailrecords
-	//
-	// Unclean shutdowns of a Matrix homeserver are likely to be fine when
-	// recovered in this manner as it's likely any lost information will be
-	// restored via federation.
-	db_opts.set_wal_recovery_mode(match config.rocksdb_recovery_mode {
-		0 => rust_rocksdb::DBRecoveryMode::AbsoluteConsistency,
-		1 => rust_rocksdb::DBRecoveryMode::TolerateCorruptedTailRecords,
-		2 => rust_rocksdb::DBRecoveryMode::PointInTime,
-		3 => rust_rocksdb::DBRecoveryMode::SkipAnyCorruptedRecord,
-		4_u8..=u8::MAX => unimplemented!(),
-	});
-
-	db_opts.set_block_based_table_factory(&block_based_options);
-	db_opts.set_env(env);
-	db_opts
 }
 
 impl KeyValueDatabaseEngine for Arc<Engine> {
@@ -145,9 +40,6 @@ impl KeyValueDatabaseEngine for Arc<Engine> {
 		let col_cache = rust_rocksdb::Cache::new_lru_cache(col_cache_capacity_bytes);
 		let db_opts = db_options(config, &db_env, &row_cache, &col_cache);
 
-		debug!("Listing column families in database");
-		let cfs = Db::<MultiThreaded>::list_cf(&db_opts, &config.database_path).unwrap_or_default();
-
 		if config.rocksdb_repair {
 			warn!("Starting database repair. This may take a long time...");
 			if let Err(e) = Db::<MultiThreaded>::repair(&db_opts, &config.database_path) {
@@ -155,21 +47,23 @@ impl KeyValueDatabaseEngine for Arc<Engine> {
 			}
 		}
 
-		debug!("Opening {} column family descriptors in database", cfs.len());
-		info!("RocksDB database compaction will take place now, a delay in startup is expected");
+		debug!("Listing column families in database");
+		let cfs = Db::<MultiThreaded>::list_cf(&db_opts, &config.database_path).unwrap_or_default();
 
+		debug!("Opening {} column family descriptors in database", cfs.len());
 		let cfds = cfs
 			.iter()
-			.map(|name| rust_rocksdb::ColumnFamilyDescriptor::new(name, db_opts.clone()))
+			.map(|name| rust_rocksdb::ColumnFamilyDescriptor::new(name, cf_options(name, db_opts.clone(), config)))
 			.collect::<Vec<_>>();
 
+		debug!("Opening database...");
 		let db = if config.rocksdb_read_only {
 			Db::<MultiThreaded>::open_cf_for_read_only(&db_opts, &config.database_path, cfs.clone(), false)?
 		} else {
 			Db::<MultiThreaded>::open_cf_descriptors(&db_opts, &config.database_path, cfds)?
 		};
 
-		debug!("Opened database at sequence number {}", db.latest_sequence_number());
+		info!("Opened database at sequence number {}", db.latest_sequence_number());
 		Ok(Arc::new(Engine {
 			rocks: db,
 			row_cache,
@@ -345,189 +239,4 @@ impl KeyValueDatabaseEngine for Arc<Engine> {
 	// TODO: figure out if this is needed for rocksdb
 	#[allow(dead_code)]
 	fn clear_caches(&self) {}
-}
-
-impl RocksDbEngineTree<'_> {
-	fn cf(&self) -> Arc<rust_rocksdb::BoundColumnFamily<'_>> { self.db.rocks.cf_handle(self.name).unwrap() }
-}
-
-impl KvTree for RocksDbEngineTree<'_> {
-	fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-
-		Ok(self.db.rocks.get_cf_opt(&self.cf(), key, &readoptions)?)
-	}
-
-	fn multi_get(
-		&self, iter: Vec<(&Arc<rust_rocksdb::BoundColumnFamily<'_>>, Vec<u8>)>,
-	) -> Vec<Result<Option<Vec<u8>>, rust_rocksdb::Error>> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-
-		self.db.rocks.multi_get_cf_opt(iter, &readoptions)
-	}
-
-	fn insert(&self, key: &[u8], value: &[u8]) -> Result<()> {
-		let writeoptions = rust_rocksdb::WriteOptions::default();
-
-		self.db
-			.rocks
-			.put_cf_opt(&self.cf(), key, value, &writeoptions)?;
-
-		if !self.db.corked() {
-			self.db.flush()?;
-		}
-
-		self.watchers.wake(key);
-
-		Ok(())
-	}
-
-	fn insert_batch(&self, iter: &mut dyn Iterator<Item = (Vec<u8>, Vec<u8>)>) -> Result<()> {
-		let writeoptions = rust_rocksdb::WriteOptions::default();
-
-		let mut batch = WriteBatchWithTransaction::<false>::default();
-
-		for (key, value) in iter {
-			batch.put_cf(&self.cf(), key, value);
-		}
-
-		let result = self.db.rocks.write_opt(batch, &writeoptions);
-
-		if !self.db.corked() {
-			self.db.flush()?;
-		}
-
-		Ok(result?)
-	}
-
-	fn remove(&self, key: &[u8]) -> Result<()> {
-		let writeoptions = rust_rocksdb::WriteOptions::default();
-
-		let result = self.db.rocks.delete_cf_opt(&self.cf(), key, &writeoptions);
-
-		if !self.db.corked() {
-			self.db.flush()?;
-		}
-
-		Ok(result?)
-	}
-
-	fn remove_batch(&self, iter: &mut dyn Iterator<Item = Vec<u8>>) -> Result<()> {
-		let writeoptions = rust_rocksdb::WriteOptions::default();
-
-		let mut batch = WriteBatchWithTransaction::<false>::default();
-
-		for key in iter {
-			batch.delete_cf(&self.cf(), key);
-		}
-
-		let result = self.db.rocks.write_opt(batch, &writeoptions);
-
-		if !self.db.corked() {
-			self.db.flush()?;
-		}
-
-		Ok(result?)
-	}
-
-	fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = (Vec<u8>, Vec<u8>)> + 'a> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-
-		Box::new(
-			self.db
-				.rocks
-				.iterator_cf_opt(&self.cf(), readoptions, rust_rocksdb::IteratorMode::Start)
-				.map(Result::unwrap)
-				.map(|(k, v)| (Vec::from(k), Vec::from(v))),
-		)
-	}
-
-	fn iter_from<'a>(&'a self, from: &[u8], backwards: bool) -> Box<dyn Iterator<Item = (Vec<u8>, Vec<u8>)> + 'a> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-
-		Box::new(
-			self.db
-				.rocks
-				.iterator_cf_opt(
-					&self.cf(),
-					readoptions,
-					rust_rocksdb::IteratorMode::From(
-						from,
-						if backwards {
-							rust_rocksdb::Direction::Reverse
-						} else {
-							rust_rocksdb::Direction::Forward
-						},
-					),
-				)
-				.map(Result::unwrap)
-				.map(|(k, v)| (Vec::from(k), Vec::from(v))),
-		)
-	}
-
-	fn increment(&self, key: &[u8]) -> Result<Vec<u8>> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-		let writeoptions = rust_rocksdb::WriteOptions::default();
-
-		let old = self.db.rocks.get_cf_opt(&self.cf(), key, &readoptions)?;
-		let new = utils::increment(old.as_deref());
-		self.db
-			.rocks
-			.put_cf_opt(&self.cf(), key, &new, &writeoptions)?;
-
-		if !self.db.corked() {
-			self.db.flush()?;
-		}
-
-		Ok(new)
-	}
-
-	fn increment_batch(&self, iter: &mut dyn Iterator<Item = Vec<u8>>) -> Result<()> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-		let writeoptions = rust_rocksdb::WriteOptions::default();
-
-		let mut batch = WriteBatchWithTransaction::<false>::default();
-
-		for key in iter {
-			let old = self.db.rocks.get_cf_opt(&self.cf(), &key, &readoptions)?;
-			let new = utils::increment(old.as_deref());
-			batch.put_cf(&self.cf(), key, new);
-		}
-
-		self.db.rocks.write_opt(batch, &writeoptions)?;
-
-		if !self.db.corked() {
-			self.db.flush()?;
-		}
-
-		Ok(())
-	}
-
-	fn scan_prefix<'a>(&'a self, prefix: Vec<u8>) -> Box<dyn Iterator<Item = (Vec<u8>, Vec<u8>)> + 'a> {
-		let mut readoptions = rust_rocksdb::ReadOptions::default();
-		readoptions.set_total_order_seek(true);
-
-		Box::new(
-			self.db
-				.rocks
-				.iterator_cf_opt(
-					&self.cf(),
-					readoptions,
-					rust_rocksdb::IteratorMode::From(&prefix, rust_rocksdb::Direction::Forward),
-				)
-				.map(Result::unwrap)
-				.map(|(k, v)| (Vec::from(k), Vec::from(v)))
-				.take_while(move |(k, _)| k.starts_with(&prefix)),
-		)
-	}
-
-	fn watch_prefix<'a>(&'a self, prefix: &[u8]) -> Pin<Box<dyn Future<Output = ()> + Send + 'a>> {
-		self.watchers.watch(prefix)
-	}
 }
