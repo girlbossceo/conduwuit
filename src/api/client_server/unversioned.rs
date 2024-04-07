8466e215c4f8fcb6efa7b4b20059a2b1a30dc0fe
strawberry <strawberry@puppygock.gay>
2024-04-07 15:45:48 -0400

    implement unstable MSC2666 support for querying mutual rooms
    https://github.com/matrix-org/matrix-spec-proposals/pull/2666
    
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/api/client_server/unversioned.rs
+++ src/api/client_server/unversioned.rs
@@ -45,10 +45,11 @@ pub async fn get_supported_versions_route(
 		unstable_features: BTreeMap::from_iter([
 			("org.matrix.e2e_cross_signing".to_owned(), true),
 			("org.matrix.msc2285.stable".to_owned(), true),
+			("uk.half-shot.msc2666.query_mutual_rooms".to_owned(), true),
 			("org.matrix.msc2836".to_owned(), true),
 			("org.matrix.msc2946".to_owned(), true),
-			("org.matrix.msc3827".to_owned(), true),
 			("org.matrix.msc3026.busy_presence".to_owned(), true),
+			("org.matrix.msc3827".to_owned(), true),
 		]),
 	};
 
