eabf2a94c99d2f2de4e0504d2635c47c6c2f626c
strawberry <strawberry@puppygock.gay>
2024-04-22 18:53:40 -0400

    add `all` admin query command for appservices
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/service/admin/query/appservice.rs
+++ src/service/admin/query/appservice.rs
@@ -13,7 +13,20 @@ pub(super) async fn appservice(subcommand: Appservice) -> Result<RoomMessageEven
 			let results = services()
 				.appservice
 				.db
-				.get_registration(appservice_id.as_ref())?;
+				.get_registration(appservice_id.as_ref());
+			let query_time = timer.elapsed();
+
+			Ok(RoomMessageEventContent::text_html(
+				format!("Query completed in {query_time:?}:\n\n```\n{:?}```", results),
+				format!(
+					"<p>Query completed in {query_time:?}:</p>\n<pre><code>{:?}\n</code></pre>",
+					results
+				),
+			))
+		},
+		Appservice::All => {
+			let timer = tokio::time::Instant::now();
+			let results = services().appservice.db.all();
 			let query_time = timer.elapsed();
 
 			Ok(RoomMessageEventContent::text_html(
