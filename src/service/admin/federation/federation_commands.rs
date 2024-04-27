61f813c18727c9f5e8538b254a92146b355991df
strawberry <strawberry@puppygock.gay>
2024-04-27 01:06:43 -0400

    admin command to get rooms a remote user is in, remove unnecessary dedupe+sort
    imagine this SQL query but in conduwuit:
    
    select * from users_in_public_rooms where user_id like '%user_id%';
    
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/service/admin/federation/federation_commands.rs
+++ src/service/admin/federation/federation_commands.rs
@@ -1,8 +1,13 @@
 use std::fmt::Write as _;
 
-use ruma::{events::room::message::RoomMessageEventContent, RoomId, ServerName};
+use ruma::{events::room::message::RoomMessageEventContent, OwnedRoomId, RoomId, ServerName, UserId};
 
-use crate::{services, utils::HtmlEscape, Result};
+use crate::{
+	service::admin::{escape_html, get_room_info},
+	services,
+	utils::HtmlEscape,
+	Result,
+};
 
 pub(crate) async fn disable_room(_body: Vec<&str>, room_id: Box<RoomId>) -> Result<RoomMessageEventContent> {
 	services().rooms.metadata.disable_room(&room_id, true)?;
@@ -69,4 +74,61 @@ pub(crate) async fn fetch_support_well_known(
 			HtmlEscape(&pretty_json)
 		),
 	))
+}
+
+pub(crate) async fn remote_user_in_rooms(_body: Vec<&str>, user_id: Box<UserId>) -> Result<RoomMessageEventContent> {
+	if user_id.server_name() == services().globals.config.server_name {
+		return Ok(RoomMessageEventContent::text_plain(
+			"User belongs to our server, please use `list-joined-rooms` user admin command instead.",
+		));
+	}
+
+	if !services().users.exists(&user_id)? {
+		return Ok(RoomMessageEventContent::text_plain(
+			"Remote user does not exist in our database.",
+		));
+	}
+
+	let mut rooms: Vec<(OwnedRoomId, u64, String)> = services()
+		.rooms
+		.state_cache
+		.rooms_joined(&user_id)
+		.filter_map(Result::ok)
+		.map(|room_id| get_room_info(&room_id))
+		.collect();
+
+	if rooms.is_empty() {
+		return Ok(RoomMessageEventContent::text_plain("User is not in any rooms."));
+	}
+
+	rooms.sort_by_key(|r| r.1);
+	rooms.reverse();
+
+	let output_plain = format!(
+		"Rooms {user_id} shares with us:\n{}",
+		rooms
+			.iter()
+			.map(|(id, members, name)| format!("{id}\tMembers: {members}\tName: {name}"))
+			.collect::<Vec<_>>()
+			.join("\n")
+	);
+	let output_html = format!(
+		"<table><caption>Rooms {user_id} shares with \
+		 us</caption>\n<tr><th>id</th>\t<th>members</th>\t<th>name</th></tr>\n{}</table>",
+		rooms
+			.iter()
+			.fold(String::new(), |mut output, (id, members, name)| {
+				writeln!(
+					output,
+					"<tr><td>{}</td>\t<td>{}</td>\t<td>{}</td></tr>",
+					escape_html(id.as_ref()),
+					members,
+					escape_html(name)
+				)
+				.unwrap();
+				output
+			})
+	);
+
+	Ok(RoomMessageEventContent::text_html(output_plain, output_html))
 }
