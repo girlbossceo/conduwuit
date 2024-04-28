9c0c4c292cca7f26ae10f54814ef12a70cc80ea5
strawberry <strawberry@puppygock.gay>
2024-04-28 18:54:27 -0400

    document hot_lib for developers a bit
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- hot_lib/src/lib.rs
+++ hot_lib/src/lib.rs
@@ -1,3 +1,6 @@
+//! hot reloadable functions, generally called by the admin room test commands
+//! see https://github.com/rksm/hot-lib-reloader-rs?tab=readme-ov-file#usage for more details if you are a dev
+
 #[no_mangle]
 pub fn test_command() {
 	println!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
--- src/service/admin/test_cmd/mod.rs
+++ src/service/admin/test_cmd/mod.rs
@@ -1,21 +1,30 @@
-use clap::Subcommand;
+//! test commands generally used for hot lib reloadable functions.
+//! see https://github.com/rksm/hot-lib-reloader-rs?tab=readme-ov-file#usage for more details if you are a dev
+
+//#[cfg(not(feature = "hot_reload"))]
+//#[allow(unused_imports)]
+//#[allow(clippy::wildcard_imports)]
+// non hot reloadable functions (?)
+//use hot_lib::*;
 #[cfg(feature = "hot_reload")]
 #[allow(unused_imports)]
 #[allow(clippy::wildcard_imports)]
-use hot_lib::*;
+use hot_lib_funcs::*;
 use ruma::events::room::message::RoomMessageEventContent;
 
 use crate::{debug_error, Result};
 
 #[cfg(feature = "hot_reload")]
 #[hot_lib_reloader::hot_module(dylib = "lib")]
-mod hot_lib {
+mod hot_lib_funcs {
+	// these will be functions from lib.rs, so `use hot_lib_funcs::test_command;`
 	hot_functions_from_file!("lib/src/lib.rs");
 }
 
 #[cfg_attr(test, derive(Debug))]
-#[derive(Subcommand)]
+#[derive(clap::Subcommand)]
 pub(crate) enum TestCommands {
+	// !admin test test1
 	Test1,
 }
 
@@ -25,6 +34,7 @@ pub(crate) async fn process(command: TestCommands, _body: Vec<&str>) -> Result<R
 			debug_error!("before calling test_command");
 			test_command();
 			debug_error!("after calling test_command");
+
 			RoomMessageEventContent::notice_plain(String::from("loaded"))
 		},
 	})
