b66d2d44d0d2a2c76bb6da634b2e4ece5df094c1
strawberry <strawberry@puppygock.gay>
2024-05-03 10:59:36 -0400

    chore: bump MSRV to 1.77.0 as 1.78.0 came out
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         file = ./rust-toolchain.toml;
 
         # See also `rust-toolchain.toml`
-        sha256 = "sha256-e4mlaJehWBymYxJGgnbuCObVlqMlQSilZ8FljG9zPHY=";
+        sha256 = "sha256-+syqAd2kX8KVa8/U2gz3blIQTTsYYt3U63xBWaGOSc8";
       };
 
       scope = pkgs: pkgs.lib.makeScope pkgs.newScope (self: {
