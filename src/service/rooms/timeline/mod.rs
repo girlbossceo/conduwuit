ffdf47d1ea34a571431b85baa00f5f77a1c5e73c
strawberry <strawberry@puppygock.gay>
2024-05-30 23:36:48 -0400

    add `latest_pdu_in_room` timeline function
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/service/rooms/timeline/mod.rs
+++ src/service/rooms/timeline/mod.rs
@@ -83,6 +83,14 @@ impl Service {
 	}
 
 	#[tracing::instrument(skip(self))]
+	pub fn latest_pdu_in_room(&self, room_id: &RoomId) -> Result<Option<Arc<PduEvent>>> {
+		self.all_pdus(user_id!("@placeholder:conduwuit.placeholder"), room_id)?
+			.last()
+			.map(|o| o.map(|(_, p)| Arc::new(p)))
+			.transpose()
+	}
+
+	#[tracing::instrument(skip(self))]
 	pub fn last_timeline_count(&self, sender_user: &UserId, room_id: &RoomId) -> Result<PduCount> {
 		self.db.last_timeline_count(sender_user, room_id)
 	}
