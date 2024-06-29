df7a1dee1524b88bcd2330334f951274d236eefe
Jason Volk <jason@zemos.net>
2024-06-29 04:18:07 +0000

    remove --features=rocksdb from smoketest; use separate target dir
    Signed-off-by: Jason Volk <jason@zemos.net>

--- tests/cargo_smoke.sh
+++ tests/cargo_smoke.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+export CARGO_TARGET_DIR="target/smokes"
 
 run () {
 	RUN_COMMAND=$@
-	echo -e "\033[1;33mTEST\033[0m $RUN_COMMAND"
+	echo -ne "\033[1;33mTEST\033[0m $RUN_COMMAND"
 	ERRORS=$($RUN_COMMAND 2>&1>/tmp/uwu_smoketest.out)
 	RESULT=$?
+	echo
 	if test $RESULT -ne 0; then
 		cat /tmp/uwu_smoketest.out
 		echo -e "$ERRORS"
@@ -44,17 +46,17 @@ element () {
 vector () {
 	TOOLCHAIN=$1; shift
 	VECTOR_OPTS=$@
-	element "$TOOLCHAIN" $VECTOR_OPTS --no-default-features --features="rocksdb"
+	element "$TOOLCHAIN" $VECTOR_OPTS --no-default-features
 	element "$TOOLCHAIN" $VECTOR_OPTS --features=default
 	element "$TOOLCHAIN" $VECTOR_OPTS --all-features
 }
 
 matrix () {
 	run cargo +nightly fmt --all --check
-	vector +nightly --profile=dev
-	vector +nightly --profile=release
 	vector "" --profile=dev
 	vector "" --profile=release
+	vector +nightly --profile=dev
+	vector +nightly --profile=release
 }
 
 BUILD=${1:-build}
