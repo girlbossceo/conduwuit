2dd68d3fa5fa48cb19864aa0f35bf825cf28d96e
Jason Volk <jason@zemos.net>
2024-07-04 12:58:53 +0000

    add time format string util
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/core/utils/time.rs
+++ src/core/utils/time.rs
@@ -1,9 +1,9 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 #[inline]
 #[must_use]
 #[allow(clippy::as_conversions)]
 pub fn now_millis() -> u64 {
-	use std::time::UNIX_EPOCH;
-
 	UNIX_EPOCH
 		.elapsed()
 		.expect("positive duration after epoch")
@@ -17,4 +17,12 @@ pub fn rfc2822_from_seconds(epoch: i64) -> String {
 	DateTime::<Utc>::from_timestamp(epoch, 0)
 		.unwrap_or_default()
 		.to_rfc2822()
+}
+
+#[must_use]
+pub fn format(ts: SystemTime, str: &str) -> String {
+	use chrono::{DateTime, Utc};
+
+	let dt: DateTime<Utc> = ts.into();
+	dt.format(str).to_string()
 }
