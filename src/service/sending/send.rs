275075a2f52ef71afb98a9749f5518c4b50cb2cf
strawberry <strawberry@puppygock.gay>
2024-07-26 00:45:23 -0400

    apply `forbidden_remote_server_names` to outbound sending and inbound federation handling
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/service/sending/send.rs
+++ src/service/sending/send.rs
@@ -1,7 +1,8 @@
 use std::{fmt::Debug, mem};
 
 use conduit::{
-	debug, debug_error, debug_warn, error::inspect_debug_log, trace, utils::string::EMPTY, Err, Error, Result,
+	debug, debug_error, debug_info, debug_warn, error::inspect_debug_log, trace, utils::string::EMPTY, Err, Error,
+	Result,
 };
 use http::{header::AUTHORIZATION, HeaderValue};
 use ipaddress::IPAddress;
@@ -29,6 +30,16 @@ impl super::Service {
 	{
 		if !self.server.config.allow_federation {
 			return Err!(Config("allow_federation", "Federation is disabled."));
+		}
+
+		if self
+			.server
+			.config
+			.forbidden_remote_server_names
+			.contains(&dest.to_owned())
+		{
+			debug_info!("Refusing to send outbound federation request to {dest}");
+			return Err!(Request(Forbidden("Federation with this homeserver is not allowed.")));
 		}
 
 		let actual = self.services.resolver.get_actual_dest(dest).await?;
