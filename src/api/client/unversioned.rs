b17589217886d8c61ff8baeac3ce06e2ff0f8615
Jason Volk <jason@zemos.net>
2024-08-29 19:17:33 +0000

    support MSC4180 unstable stable flag for MSC3916
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/api/client/unversioned.rs
+++ src/api/client/unversioned.rs
@@ -53,6 +53,8 @@ pub(crate) async fn get_supported_versions_route(
 			("org.matrix.msc3026.busy_presence".to_owned(), true), /* busy presence status (https://github.com/matrix-org/matrix-spec-proposals/pull/3026) */
 			("org.matrix.msc3827".to_owned(), true), /* filtering of /publicRooms by room type (https://github.com/matrix-org/matrix-spec-proposals/pull/3827) */
 			("org.matrix.msc3575".to_owned(), true), /* sliding sync (https://github.com/matrix-org/matrix-spec-proposals/pull/3575/files#r1588877046) */
+			("org.matrix.msc3916.stable".to_owned(), true), /* authenticated media (https://github.com/matrix-org/matrix-spec-proposals/pull/3916) */
+			("org.matrix.msc4180".to_owned(), true), /* stable flag for 3916 (https://github.com/matrix-org/matrix-spec-proposals/pull/4180) */
 		]),
 	};
 
