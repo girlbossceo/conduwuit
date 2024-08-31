5dbb868936bc1c96ff2671396389c3078d18a17c
strawberry <strawberry@puppygock.gay>
2024-08-31 12:18:21 -0400

    remove unnecessary loops/allocations in client `/capabilities`
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/api/client/capabilities.rs
+++ src/api/client/capabilities.rs
@@ -1,8 +1,11 @@
 use std::collections::BTreeMap;
 
 use axum::extract::State;
-use ruma::api::client::discovery::get_capabilities::{
-	self, Capabilities, RoomVersionStability, RoomVersionsCapability, ThirdPartyIdChangesCapability,
+use ruma::{
+	api::client::discovery::get_capabilities::{
+		self, Capabilities, RoomVersionStability, RoomVersionsCapability, ThirdPartyIdChangesCapability,
+	},
+	RoomVersionId,
 };
 
 use crate::{Result, Ruma};
@@ -14,13 +17,19 @@ use crate::{Result, Ruma};
 pub(crate) async fn get_capabilities_route(
 	State(services): State<crate::State>, _body: Ruma<get_capabilities::v3::Request>,
 ) -> Result<get_capabilities::v3::Response> {
-	let mut available = BTreeMap::new();
-	for room_version in &services.globals.unstable_room_versions {
-		available.insert(room_version.clone(), RoomVersionStability::Unstable);
-	}
-	for room_version in &services.globals.stable_room_versions {
-		available.insert(room_version.clone(), RoomVersionStability::Stable);
-	}
+	let available: BTreeMap<RoomVersionId, RoomVersionStability> = services
+		.globals
+		.unstable_room_versions
+		.iter()
+		.map(|unstable_room_version| (unstable_room_version.clone(), RoomVersionStability::Unstable))
+		.chain(
+			services
+				.globals
+				.stable_room_versions
+				.iter()
+				.map(|stable_room_version| (stable_room_version.clone(), RoomVersionStability::Stable)),
+		)
+		.collect();
 
 	let mut capabilities = Capabilities::default();
 	capabilities.room_versions = RoomVersionsCapability {
@@ -28,7 +37,7 @@ pub(crate) async fn get_capabilities_route(
 		available,
 	};
 
-	// conduit does not implement 3PID stuff
+	// we do not implement 3PID stuff
 	capabilities.thirdparty_id_changes = ThirdPartyIdChangesCapability {
 		enabled: false,
 	};
