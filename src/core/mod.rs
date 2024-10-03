6f7ea3ae62665741926da38e33011613a5fbf830
Jason Volk <jason@zemos.net>
2024-10-03 01:00:00 +0000

    add mactors for true/false
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -38,3 +38,59 @@ pub mod mods {
 		() => {};
 	}
 }
+
+/// Functor for falsy
+#[macro_export]
+macro_rules! is_false {
+	() => {
+		|x| !x
+	};
+}
+
+/// Functor for truthy
+#[macro_export]
+macro_rules! is_true {
+	() => {
+		|x| !!x
+	};
+}
+
+/// Functor for equality to zero
+#[macro_export]
+macro_rules! is_zero {
+	() => {
+		$crate::is_matching!(0)
+	};
+}
+
+/// Functor for equality i.e. .is_some_and(is_equal!(2))
+#[macro_export]
+macro_rules! is_equal_to {
+	($val:expr) => {
+		|x| x == $val
+	};
+}
+
+/// Functor for less i.e. .is_some_and(is_less_than!(2))
+#[macro_export]
+macro_rules! is_less_than {
+	($val:expr) => {
+		|x| x < $val
+	};
+}
+
+/// Functor for matches! i.e. .is_some_and(is_matching!('A'..='Z'))
+#[macro_export]
+macro_rules! is_matching {
+	($val:expr) => {
+		|x| matches!(x, $val)
+	};
+}
+
+/// Functor for !is_empty()
+#[macro_export]
+macro_rules! is_not_empty {
+	() => {
+		|x| !x.is_empty()
+	};
+}
