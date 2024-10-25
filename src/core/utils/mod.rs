cf59f738b9f687aa0902bf2bd011219567e4fea5
Jason Volk <jason@zemos.net>
2024-10-25 02:01:29 +0000

    move macros incorrectly moved out of utils to top level
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/core/utils/mod.rs
+++ src/core/utils/mod.rs
@@ -57,3 +57,71 @@ macro_rules! at {
 		|t| t.$idx
 	};
 }
+
+/// Functor for equality i.e. .is_some_and(is_equal!(2))
+#[macro_export]
+macro_rules! is_equal_to {
+	($val:ident) => {
+		|x| x == $val
+	};
+
+	($val:expr) => {
+		|x| x == $val
+	};
+}
+
+/// Functor for less i.e. .is_some_and(is_less_than!(2))
+#[macro_export]
+macro_rules! is_less_than {
+	($val:ident) => {
+		|x| x < $val
+	};
+
+	($val:expr) => {
+		|x| x < $val
+	};
+}
+
+/// Functor for equality to zero
+#[macro_export]
+macro_rules! is_zero {
+	() => {
+		$crate::is_matching!(0)
+	};
+}
+
+/// Functor for matches! i.e. .is_some_and(is_matching!('A'..='Z'))
+#[macro_export]
+macro_rules! is_matching {
+	($val:ident) => {
+		|x| matches!(x, $val)
+	};
+
+	($val:expr) => {
+		|x| matches!(x, $val)
+	};
+}
+
+/// Functor for !is_empty()
+#[macro_export]
+macro_rules! is_not_empty {
+	() => {
+		|x| !x.is_empty()
+	};
+}
+
+/// Functor for truthy
+#[macro_export]
+macro_rules! is_true {
+	() => {
+		|x| !!x
+	};
+}
+
+/// Functor for falsy
+#[macro_export]
+macro_rules! is_false {
+	() => {
+		|x| !x
+	};
+}
