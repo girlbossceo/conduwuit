1606441d090952a7806140f060a0d1d7b34f94b9
strawberry <strawberry@puppygock.gay>
2024-11-20 20:23:13 -0500

    disable admin_room_notices for complement, update welcome text
    Signed-off-by: strawberry <strawberry@puppygock.gay>

--- src/service/admin/grant.rs
+++ src/service/admin/grant.rs
@@ -77,18 +77,20 @@ pub async fn make_user_admin(&self, user_id: &UserId) -> Result<()> {
 		}
 	}
 
-	let welcome_message = String::from("## Thank you for trying out conduwuit!\n\nconduwuit is a fork of upstream Conduit which is in Beta. This means you can join and participate in most Matrix rooms, but not all features are supported and you might run into bugs from time to time.\n\nHelpful links:\n> Git and Documentation: https://github.com/girlbossceo/conduwuit\n> Report issues: https://github.com/girlbossceo/conduwuit/issues\n\nFor a list of available commands, send the following message in this room: `!admin --help`\n\nHere are some rooms you can join (by typing the command):\n\nconduwuit room (Ask questions and get notified on updates):\n`/join #conduwuit:puppygock.gay`");
+	if self.services.server.config.admin_room_notices {
+		let welcome_message = String::from("## Thank you for trying out conduwuit!\n\nconduwuit is technically a hard fork of Conduit, which is in Beta. The Beta status initially was inherited from Conduit, however overtime this Beta status is rapidly becoming less and less relevant as our codebase significantly diverges more and more. conduwuit is quite stable and very usable as a daily driver and for a low-medium sized homeserver. There is still a lot of more work to be done, but it is in a far better place than the project was in early 2024.\n\nHelpful links:\n> GitHub Repo: https://github.com/girlbossceo/conduwuit\n> Documentation: https://conduwuit.puppyirl.gay/\n> Report issues: https://github.com/girlbossceo/conduwuit/issues\n\nFor a list of available commands, send the following message in this room: `!admin --help`\n\nHere are some rooms you can join (by typing the command into your client) -\n\nconduwuit space: `/join #conduwuit-space:puppygock.gay`\nconduwuit main room (Ask questions and get notified on updates): `/join #conduwuit:puppygock.gay`\nconduwuit offtopic room: `/join #conduwuit-offtopic:puppygock.gay`");
 
-	// Send welcome message
-	self.services
-		.timeline
-		.build_and_append_pdu(
-			PduBuilder::timeline(&RoomMessageEventContent::text_markdown(welcome_message)),
-			server_user,
-			&room_id,
-			&state_lock,
-		)
-		.await?;
+		// Send welcome message
+		self.services
+			.timeline
+			.build_and_append_pdu(
+				PduBuilder::timeline(&RoomMessageEventContent::text_markdown(welcome_message)),
+				server_user,
+				&room_id,
+				&state_lock,
+			)
+			.await?;
+	}
 
 	Ok(())
 }
