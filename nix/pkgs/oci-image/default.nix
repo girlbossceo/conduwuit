c880005a0ea31d70c60ea581dcbe27ece6cae43e
Jade Ellis <jade@ellis.link>
2025-01-19 11:22:12 +0000

    fixup! Add OCI labels to image

--- nix/pkgs/oci-image/default.nix
+++ nix/pkgs/oci-image/default.nix
@@ -29,10 +29,10 @@ dockerTools.buildLayeredImage {
       "RUST_BACKTRACE=full"
     ];
     Labels = {
-      org.opencontainers.image.title = main.pname;
-      org.opencontainers.image.version = main.version;
-      org.opencontainers.image.revision = inputs.self.rev or inputs.self.dirtyRev or "";
-      org.opencontainers.image.created = "@${toString inputs.self.lastModified}";
+      "org.opencontainers.image.title" = main.pname;
+      "org.opencontainers.image.version" = main.version;
+      "org.opencontainers.image.revision" = inputs.self.rev or inputs.self.dirtyRev or "";
+      "org.opencontainers.image.created" = builtins.formatTime "%Y-%m-%dT%H:%M:%SZ" inputs.self.lastModified
     };
   };
 }
